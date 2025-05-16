f143e157f5b015f25546e2c3fc826c0263d7d5c9
ahmednazir <ahmednazir333@gmail.com>
2025-05-16 13:08:24 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline text-center text-red-600">
+                Hello world!
+            </h1>
+        </>
+    );
+}
+
+export default App;
